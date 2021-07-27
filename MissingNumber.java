f57224aec6fc004d69571ceb9d7706a6a80e80de
Gabriel <gsechaus@gmail.com>
2021-07-27 10:22:40 -0300

    :art: Added comment to MissingNumber.java
    Added a comment to first solution in missing number problem

--- MissingNumber.java
+++ MissingNumber.java
@@ -5,6 +5,12 @@ public class MissingNumber {
     int[] nums = {0,1,3,4};
     System.out.println(whichIsMissing(nums));
   }
+  
+  /* 
+    Primeira solução, loopa pela array nums e verifica se o index é igual ao número,
+    se não for então ele pega o index e assume que esse é o numero que está faltando
+    na array
+  */
 
   static int whichIsMissing(int[] nums) { 
     Arrays.sort(nums);
