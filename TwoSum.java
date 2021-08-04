6829b026c5925305700b219892a344e8270e11e3
Gabriel <gsechaus@gmail.com>
2021-08-04 09:47:54 -0300

    :sparkles: Added two sum pattern

---
+++ TwoSum.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class TwoSum {
+  public static void main(String[] args) {
+    int[] nums = { 0, 5, 3, 4 };
+    int target = 9;
+
+    System.out.println(Arrays.toString(findTwoSum1(nums, target)));
+  }
+
+  static int[] findTwoSum1(int[] nums, int target){
+    int[] twoSum = {0,0}; 
+
+    for(int i = 0; i < nums.length; i++){ 
+      for(int j = 0; j < nums.length; j++){ 
+        int sum = nums[i] + nums[j];
+        if(nums[i] != nums[j] && sum == target){
+          twoSum[0] = i;
+          twoSum[1] = j;
+          return twoSum;          
+        }
+      }
+    }
+
+    return twoSum;
+  }
+
+  static int[] findTwoSum2(int[] nums, int target){
+    int[] twoSum = {0,0}; 
+
+    for(int i = 0; i < nums.length; i++){ 
+      
+    }
+    
+    return twoSum;
+  }
+}
